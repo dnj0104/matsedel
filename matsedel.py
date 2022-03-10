20a48a4e94d4abeb6c819ab8aec04e75f02338e7
dnj0104 <54766266+dnj0104@users.noreply.github.com>
2022-03-10 21:09:27 +0100

    Add files via upload

---
+++ matsedel.py
@@ -0,0 +1,36 @@
+
+print("Hej!!! \nVälkommen till matsedeln")
+
+name = input("Vad heter du idag? ")
+
+
+print("Hejsan " + name + " tack för att du tittar in idag" )
+
+print("Vad vill du göra?\nVill du veta veckans mat?")
+ 
+answer = input("svara JA eller NEJ ") 
+
+menu = "Måndag: Tjurhjärna med bea och ris,\nTisdag: fläsksvål och katarr,\nOnsdag: Gammalt ris"
+
+print("\nOkej, du valde "  + answer +  " här kommer veckans matsedel för huset du bor i ")
+
+print(menu)
+
+url1 = "https://skolmaten.se/sannaskolan2"
+url2 = "https://skolmaten.se/ronnowskolan/"
+
+print("vill du veta vilken mat som serveras i skolan")
+print("tryck här för Sånna " +  url1)
+print("tryck här för Rönnow " + url2)
+
+
+
+
+
+
+
+
+
+
+
+
